6ecf3b3ec170f4441d544f57014fa0831b5748da
sanketgupta71 <sanketgupta71@gmail.com>
2020-09-19 16:33:28 +0530

    added fn7

---
+++ fn7.js
@@ -0,0 +1,34 @@
+//Destructuring- breaking down a complex structure 
+//into simpler parts. 
+arr = [1,2,3];
+var[a,b,c] = arr;
+console.log(a);
+var a,b = arr;
+console.log('s',b);
+var[a,,c] = arr;
+console.log(c);
+
+//object destructuring with same variable name
+const{username,city}={username:'Raju',city:'Bangalore'}
+console.log(username,city);
+
+//object destructuring with diff variable name
+const{username:myname,city:mycity}={username:'Raju',city:'Bangalore'}
+console.log(myname,mycity);
+
+var student={
+    name:'Raju',city:'Chennai',hobby:['Sports','Music']
+}
+
+var details = (s)=>{
+    console.log(s.hobby)
+}
+details(student);
+var ndetails = ({name,city,hobby})=>{
+    console.log(hobby)
+}
+ndetails(student);
+
+var firstName = 'Kumaran'
+let greeting= `Hi ${firstName}`;
+console.log(greeting)
